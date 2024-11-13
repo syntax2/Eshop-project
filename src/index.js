6097786ac838847458b02b79b4a128097e67edfd
Ashish Kadian <akadian135@gmail.com>
2024-11-13 10:42:20 +0530

    minor changes for the index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import useAuthentication from "./hooks/useAuthentication";
+import useServices from "./hooks/useServices";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ConnectedApp = () => {
+  const { AuthProvider } = useAuthentication();
+  const { ServicesProvider } = useServices();
+  return (
+    <AuthProvider>
+      <ServicesProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ServicesProvider>
+    </AuthProvider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ConnectedApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
